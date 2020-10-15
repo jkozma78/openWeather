d79a8973b544fa8a86949dd830328e9fa6a03142
jkozma78 <67119975+jkozma78@users.noreply.github.com>
2020-10-15 17:06:41 +0200

    geo

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ let units = "metric";
 let lang = "hu";
 let lat = "47.5";
 let lon = "19.08333"
-let latLon = [47.5, 19.08];
-getLocation();
-console.log(latLon)
+//let latLon = [47.56, 18.34];
+//let latLon = [47.5, 19.08];
+let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latLon[0] + "&lon=" + latLon[1] + "&appid=" + appId + "&units=" + units + "&lang=" + lang;
+getLocation();
+
+//let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latLon[0] + "&lon=" + latLon[1] + "&appid=" + appId + "&units=" + units + "&lang=" + lang;
 
 function getServerData(url) {
     let fetchOptions = {
@@ -23,6 +25,7 @@ function getServerData(url) {
     );
 }
 
+function getd(){
 getServerData(url).then(
     data => {
         console.log(data)
@@ -32,6 +35,7 @@ getServerData(url).then(
 
 
 );
+};
 
 function getLocation() {
     if (navigator.geolocation != "") {
@@ -40,6 +44,7 @@ function getLocation() {
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
+    
 };
 
 function showPosition(position) {
@@ -47,40 +52,43 @@ function showPosition(position) {
     //     "Longitude: " + position.coords.longitude);
     latLon = [position.coords.latitude, position.coords.longitude];
     console.log(latLon);
-    return latLon
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latLon[0] + "&lon=" + latLon[1] + "&appid=" + appId + "&units=" + units + "&lang=" + lang;
+    console.log(url)
+    getd()
+    return url
 };
 
 
 function createWeather() {
-    weatherObj.alerts ? weatherAlerts():document.querySelector("#alert").innerHTML="Nincs riasztás";
+    weatherObj.alerts ? weatherAlerts() : document.querySelector("#alert").innerHTML = "Nincs riasztás";
     foreCast();
     hourly();
 
 
     let rain = 0;
     for (let r in weatherObj.minutely) {
-        rain = rain + weatherObj.minutely[r].precipitation
+        rain = rain + weatherObj.minutely[r].precipitation / 24
     };
 
-    
+
     document.querySelector("h1").innerHTML = getTimeFromEpox(weatherObj.current.dt);
     document.querySelector("h4").innerHTML = "A következő órában várható eső: " + rain.toFixed(1) + " mm";
-    
+
     let ikon = document.querySelector(".icon");
     let attr = ("img/openweather/" + weatherObj.current.weather[0].icon + "@2x.png");
     ikon.setAttribute("src", attr);
     ikon.setAttribute("title", weatherObj.current.weather[0].description)
-    
+
     let a = document.querySelector("#temp");
     a.innerHTML = weatherObj.timezone + " " + weatherObj.current.temp.toFixed() + `&#8451`;
-    
+
     let b = document.querySelector("#sunrise");
     let sunriseHour = new Date(weatherObj.current.sunrise * 1000).getHours();
     sunriseHour = frontZero(sunriseHour);
     let sunriseMinute = new Date(weatherObj.current.sunrise * 1000).getMinutes();
     sunriseMinute = frontZero(sunriseMinute);
     b.innerHTML = "Napkelte:" + " " + sunriseHour + ":" + sunriseMinute;
-    
+
     let c = document.querySelector("#sunset");
     let sunsetHour = new Date(weatherObj.current.sunset * 1000).getHours();
     sunsetHour = frontZero(sunsetHour);
@@ -105,32 +113,32 @@ function frontZero(time) {
     return time;
 };
 
-function foreCast(){
-    for (let k in weatherObj.daily){
-    bt = document.createElement("div");
-    bt.setAttribute("class", "col border rounded border-success");
-    document.querySelector("#foreCast").appendChild(bt);
-    bt.innerHTML = weatherObj.daily[k].temp.day.toFixed()+ `&#8451`;
-    imgs=document.createElement("img");
-    imgs.setAttribute("class","figure-img img-fluid rounded icon rounded border border-danger data-toggle='tooltip'");
-    imgs.setAttribute("src", "img/openweather/" + weatherObj.daily[k].weather[0].icon + "@2x.png")
-    bt.appendChild(imgs)
+function foreCast() {
+    for (let k in weatherObj.daily) {
+        bt = document.createElement("div");
+        bt.setAttribute("class", "col border rounded border-success");
+        document.querySelector("#foreCast").appendChild(bt);
+        bt.innerHTML = weatherObj.daily[k].temp.day.toFixed() + `&#8451` + " " + dayNames[new Date(weatherObj.daily[k].dt * 1000).getUTCDay()];
+        imgs = document.createElement("img");
+        imgs.setAttribute("class", "figure-img img-fluid rounded icon rounded border border-danger data-toggle='tooltip'");
+        imgs.setAttribute("src", "img/openweather/" + weatherObj.daily[k].weather[0].icon + "@2x.png")
+        bt.appendChild(imgs)
     };
 };
 
-function weatherAlerts(){
-    document.querySelector("#alert").innerHTML=weatherObj.alerts[0].event
+function weatherAlerts() {
+    document.querySelector("#alert").innerHTML = weatherObj.alerts[0].event
     document.querySelector(".card").innerHTML = weatherObj.alerts[0].description
 };
 
-function hourly(){
-    for (let k in weatherObj.hourly){
+function hourly() {
+    for (let k in weatherObj.hourly) {
         bt = document.createElement("div");
         bt.setAttribute("class", "col-1 border rounded border-warning");
         document.querySelector("#hourly").appendChild(bt);
-        bt.innerHTML = getTimeFromEpox(weatherObj.hourly[k].dt)+"<br>"+ weatherObj.hourly[k].temp.toFixed()+ `&#8451`;
-        imgs=document.createElement("img");
-        imgs.setAttribute("class","figure-img img-fluid rounded icon rounded border border-danger data-toggle='tooltip'");
+        bt.innerHTML = getTimeFromEpox(weatherObj.hourly[k].dt) + "<br>" + weatherObj.hourly[k].temp.toFixed() + `&#8451`;
+        imgs = document.createElement("img");
+        imgs.setAttribute("class", "figure-img img-fluid rounded icon rounded border border-danger data-toggle='tooltip'");
         imgs.setAttribute("src", "img/openweather/" + weatherObj.hourly[k].weather[0].icon + "@2x.png")
         bt.appendChild(imgs)
     };
